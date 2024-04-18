9ea948bd9fd70c8ea78d8475ad0d7dd1dddf6cf3
divanster <divanster@gmail.com>
2024-04-18 20:56:48 +0300

    adding picture and more model information, navbar, styles

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'food'
 urlpatterns = [
+    #/food/
     path('', views.index, name='index'),
-    path('item/', views.item, name='item')
+    #/food/1
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('item/', views.item, name='item'),
+
 ]
