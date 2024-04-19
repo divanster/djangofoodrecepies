ded31599af62a3495822868fdd03b31b7d1f8648
divanster <divanster@gmail.com>
2024-04-19 21:42:27 +0300

    addind item and updating

--- food/urls.py
+++ food/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
     # add items
-    path('add/', views.create_item, name='create_item')
+    path('add/', views.create_item, name='create_item'),
+    # edit
+    path('update/<int:item_id>/', views.update_item, name='update_item'),
+
+
 
 ]
--- food/views.py
+++ food/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ItemForm
 from .models import Item
 from django.template import loader
 
@@ -23,3 +25,29 @@ def detail(request, item_id):
     }
 
     return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+
+    return render(request, 'food/item-form.html', {'form': form})
+
+
+def update_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+
+    return render(request, 'food/item-form.html', {'form': form, 'item_id': item_id})
+
+
+
+
+
+
