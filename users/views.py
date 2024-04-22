706113b3158d1ff2820f6b8e6ed2711210cc2c69
divanster <divanster@gmail.com>
2024-04-22 18:11:20 +0300

    creating user login form

--- users/views.py
+++ users/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
+# from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'user/register.html', {'form': form})
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, your account has been created!')
+            return redirect('food:index')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
-    # if request.method == 'POST':
+
