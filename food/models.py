6a8ef3f346997d1ea365cee06a518ce030d88a7b
divanster <divanster@gmail.com>
2024-05-13 12:57:46 +0300

    before adding rating and views

--- food/models.py
+++ food/models.py
@@ -12,10 +12,11 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    # item_price = models.DecimalField(max_digits=10, decimal_places=2) better for prices
     # item_image = models.ImageField(upload_to='') better for image storage with validation and upload function
     item_image = models.CharField(max_length=500,
-                                  default='https://media.istockphoto.com/id/1426890025/es/foto/la-pizza-de-la-que-te-olvidaste-durante-una-semana-y-se-puso-mohosa.jpg?s=612x612&w=0&k=20&c=r0cHrYxEjoLUoSz9VQCglgXc6Win_fFu-fjDwWfoPu4=')
+                                  default='https://media.istockphoto.com/id/1426890025/es/foto/la-pizza-de-la-que-te'
+                                          '-olvidaste-durante-una-semana-y-se-puso-mohosa.jpg?s=612x612&w=0&k=20&c'
+                                          '=r0cHrYxEjoLUoSz9VQCglgXc6Win_fFu-fjDwWfoPu4=')
 
     def get_absolute_url(self):
         return reverse("food:detail", kwargs={"pk": self.pk})
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+
 # from django.utils.translation import gettext as _
 #
 #
@@ -21,8 +23,8 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterForm(UserCreationForm):
     username = forms.CharField(label='Username', widget=forms.TextInput())
     email = forms.EmailField(label='Email')
-    password1 = forms.CharField(label='Password')
-    password2 = forms.CharField(label='Confirm Password')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
